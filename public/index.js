8cab65237a92acf3c2b2759f409589059f7d6ec8
Tanner Rogers <trroger2@alumni.ncsu.edu>
2023-12-11 11:36:51 -0500

    assignment

--- public/index.js
+++ public/index.js
@@ -4,6 +4,59 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let response = await fetch('https://api.twelvedata.com/time_series?start_date=2023-11-08&2023-12-08&symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=dc025c5e36a944c2b42665e3ddea6c31');
+
+    let results = await response.json()
+
+    let GME = results.GME
+    let MSFT = results.MSFT
+    let DIS = results.DIS
+    let BNTX = results.BNTX
+
+    const stocks = [GME, MSFT, DIS, BNTX]  
+
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    } 
+
+    stocks.forEach(stock => stock.values.reverse())
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.reverse().map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol)
+            }))
+        }
+    }); 
+
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks,
+            datasets: stocks.map(stock => ({
+                label: 'Highest',
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol)
+            }))
+        }
+    });
 }
 
 main()
